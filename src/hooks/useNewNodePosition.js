f6a11b9ddb22595205c7b2d085f63f094678377b
Martin Simpson <martins93@gmail.com>
2023-03-22 03:24:03 +0100

    add nodes from bottom of awareness node

--- src/hooks/useNewNodePosition.js
+++ src/hooks/useNewNodePosition.js
@@ -2,15 +2,28 @@ import { useCallback } from 'react'
 import { useStoreApi } from 'reactflow'
 import { getViewportCenter } from '../util/getViewportCenter'
 import { grid } from '../components/LiveFlow'
+import { useAwareness } from './useAwareness'
 
 export function useNewNodePosition() {
   const rfStore = useStoreApi()
+  const awareness = useAwareness()
 
-  const getPos = useCallback((width,height) => {
+  const getCenterPos = useCallback((width,height) => {
     let center = getViewportCenter(rfStore.getState(), grid)
 
     return { x: center.x - width, y: center.y - height }
   }, [rfStore])
+
+  const getPos = useCallback((w,h)=>{
+    if (awareness.getLocalState()?.position && awareness.getLocalState()?.height) {
+      return {
+        x: awareness.getLocalState().position.x,
+        y: awareness.getLocalState().position.y + awareness.getLocalState().height + 15
+      }
+    } else {
+      return getCenterPos(w,h)
+    }
+  })
 
   return getPos
 }
