c23c51706beb43a02307f14418a29b6cd323ce57
Martin Simpson <martins93@gmail.com>
2023-03-23 13:36:07 +0100

    only connect to hocuspocus server if agora link is valid

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AgoraApp from './AgoraApp'
-
 import './reset.css'
 import './main.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <AgoraApp />
-  // </React.StrictMode>
-)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './AgoraApp'
