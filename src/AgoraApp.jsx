a221a38e66a8823222b2f0df88a85cccda4c2027
Martin Simpson <martins93@gmail.com>
2023-03-23 13:52:37 +0100

    fix agora hatcher

--- src/AgoraApp.jsx
+++ src/AgoraApp.jsx
@@ -1,33 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 import { AgoraView } from "./components/AgoraView"
-import { PasswordGate } from "./components/PasswordGate";
+import { PasswordGate } from "./components/PasswordGate"
+import { hatchAgora } from './agoraHatcher'
+
+import './main.css'
+
 
 const urlParams = new URLSearchParams(window.location.search);
 export const backstageEnabled = urlParams.has('backstage')
 const base = urlParams.get('agora')
 
-function init(base, hocuspocusurl) {
-  const baseAgora = new agoraHatcher.Agora(base, hocuspocusurl)
-  
-  const validSpaces = ['space00', 'space01', 'space02', 'space03', 'space04', 'space05']
-  const spaceCount = 6
-  
-  const spaces = validSpaces.slice(0, spaceCount).map(space=>new agoraHatcher.Space(space, baseAgora)) 
-
-  return {
-    baseAgora,
-    spaces
-  }
-}
-
 if (base) {
-  const { baseAgora, spaces } = init(base, 'wss://hocuspocus.taat.live')
+  const { baseAgora, spaces } = hatchAgora(base, 'wss://hocuspocus.taat.live')
 
   ReactDOM.createRoot(document.getElementById('root')).render(
-    // <React.StrictMode>
-    <PasswordGate>
-      <AgoraView agora={baseAgora} spaces={spaces}/>
-    </PasswordGate>
-    // </React.StrictMode>
+    <React.StrictMode>
+      <PasswordGate>
+        <AgoraView agora={baseAgora} spaces={spaces}/>
+      </PasswordGate>
+    </React.StrictMode>
   )
 } else {
   document.getElementById('root').innerHTML = `
