04f2d8c855d764bad8ef68b4393e29a3bfa2b66d
Martin Simpson <martins93@gmail.com>
2023-03-23 13:36:07 +0100

    only connect to hocuspocus server if agora link is valid

--- src/AgoraApp.jsx
+++ src/AgoraApp.jsx
@@ -1,32 +1,39 @@
 import { AgoraView } from "./components/AgoraView"
-import agoraHatcher from "./agoraHatcher";
 import { PasswordGate } from "./components/PasswordGate";
 
-const hocuspocusurl = 'wss://hocuspocus.taat.live' //'ws://localhost:3000'
-
-const validSpaces = ['space00', 'space01', 'space02', 'space03', 'space04', 'space05']
 const urlParams = new URLSearchParams(window.location.search);
-
+export const backstageEnabled = urlParams.has('backstage')
 const base = urlParams.get('agora')
 
-const baseAgora = new agoraHatcher.Agora(base, hocuspocusurl)
-//const spaceCount = !urlParams.has('spaces') ? 1 : Math.min(Math.max(1, parseInt(urlParams.get('spaces'))), validSpaces.length)
-const spaceCount = 6
-
-const spaces = validSpaces.slice(0, spaceCount).map(space=>new agoraHatcher.Space(space, baseAgora)) 
-
-export const backstageEnabled = urlParams.has('backstage')
-//const spaces = validSpaces
-  //.filter(space=>urlParams.has(space))
-  //.map(space=>new agoraHatcher.Space(space, baseAgora)) 
-
-export default function AgoraApp() {
-  if (spaces.length < 1 || !base)
-    return (<p>Invalid url</p>)
+function init(base, hocuspocusurl) {
+  const baseAgora = new agoraHatcher.Agora(base, hocuspocusurl)
   
-  return (
-    //<PasswordGate>
-      <AgoraView agora={baseAgora} spaces={spaces}/>
-    //</PasswordGate>
-  )
+  const validSpaces = ['space00', 'space01', 'space02', 'space03', 'space04', 'space05']
+  const spaceCount = 6
+  
+  const spaces = validSpaces.slice(0, spaceCount).map(space=>new agoraHatcher.Space(space, baseAgora)) 
+
+  return {
+    baseAgora,
+    spaces
+  }
 }
+
+if (base) {
+  const { baseAgora, spaces } = init(base, 'wss://hocuspocus.taat.live')
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    // <React.StrictMode>
+    <PasswordGate>
+      <AgoraView agora={baseAgora} spaces={spaces}/>
+    </PasswordGate>
+    // </React.StrictMode>
+  )
+} else {
+  document.getElementById('root').innerHTML = `
+  <div style="height: 100%; display: flex; align-items: center; justify-content: center; ">
+    link is invalid!
+  </div>
+  `
+}
+
