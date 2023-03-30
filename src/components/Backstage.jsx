b39606e24e1028b47197cefdcf62d77689dab2b9
Martin Simpson <martins93@gmail.com>
2023-03-30 21:31:38 +0200

    refine backstage tab

--- src/components/Backstage.jsx
+++ src/components/Backstage.jsx
@@ -3,8 +3,20 @@ import { useYkv } from '../hooks/useYkv'
 import { YkvTextInput, YkvCheckbox } from './YkvUi'
 
 export function Backstage() {
+  const {name} = useAgora()
+
+  const publicLink = `https://preview.taat.live/?agora=${name}`
+  const backstageLink = `${publicLink}&backstage`
+  
   return (
     <div style={{height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-evenly'}}>
+      <DashboardBox>
+        <p>Welcome backstage</p>
+        <p>Please take care</p>
+        <p>links:</p>
+        <p><a href={publicLink}>public</a></p>
+        <p><a href={backstageLink}>backstage</a></p>
+      </DashboardBox>
       <SpaceListPanel/>
       <MiscMetadataPanel/>
     </div>
@@ -25,7 +37,7 @@ function MiscMetadataPanel() {
 
   return (
     <DashboardBox>
-      <h2>liveAV</h2>
+      <h2>{agora.name}/LiveAV</h2>
       <YkvTextInput ykey={'liveAV/roomID'} state={state} metadataYkv={ykv}/>
     </DashboardBox>
   )
@@ -37,7 +49,7 @@ function SpaceListPanel() {
 
   return (
     <DashboardBox>
-      
+      <h2>{agora.name}/metadata</h2>
       <YkvCheckbox ykey={`infopage-enabled`} state={state} metadataYkv={ykv}/><br/>
           {
             state[`infopage-enabled`]?.val && <>
@@ -48,12 +60,15 @@ function SpaceListPanel() {
       {
       ['space00', 'space01', 'space02', 'space03', 'space04', 'space05'].map((s,i) =>
         <>
-          {/* <h2 key={i}>{s}</h2> */}
+          {/* <p key={i}>{s}</p> */}
           <YkvCheckbox ykey={`${s}-enabled`} state={state} metadataYkv={ykv} key={i}/>
           <br/>
           {
             state[`${s}-enabled`]?.val && <>
               <YkvTextInput label={'name'} ykey={`${s}-displayName`} state={state} metadataYkv={ykv} key={i}/>
+              <YkvCheckbox label={'public'} ykey={`${s}-public`} state={state} metadataYkv={ykv} key={i}/>
+              {state[`${s}-public`]?.val && <YkvCheckbox label={'publicEditable'} ykey={`${s}-publicEditable`} state={state} metadataYkv={ykv} key={i}/>}
+              
             </>
           }
           <hr/>
