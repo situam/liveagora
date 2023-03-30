a2fdfde5fe3260b1b1e3d8c836905136e98dac32
Martin Simpson <martins93@gmail.com>
2023-03-31 00:01:26 +0200

    reenable infopage, refine dashboard

--- src/components/AgoraView.jsx
+++ src/components/AgoraView.jsx
@@ -13,37 +13,33 @@ import { Backstage } from "./Backstage"
 export function AgoraView({agora, spaces}) {
   const { infoPage, cfgSpaces } = useCfgSpaces(agora, spaces)
 
-  const cfgTitles = cfgSpaces.map(s=>s.displayName || s.name)
-  const cfgBodies = cfgSpaces.map((s,i)=>
-    <SpaceProvider space={s} key={i}>
-      <GatedSpaceFlow/>
-    </SpaceProvider>
-  )
+  const titles =
+    [
+      backstageEnabled && <em>backstage</em>,
+      infoPage && <em>{infoPage}</em>, 
+      ...cfgSpaces.map(s=>s.displayName || s.name)
+    ]
+    .filter(el=>el)
+
+  const bodies =
+    [
+      backstageEnabled && <Backstage/>,
+      infoPage && <InfoPage/>, 
+      ...cfgSpaces.map((s,i)=>
+        <SpaceProvider space={s} key={i}>
+          <GatedSpaceFlow/>
+        </SpaceProvider>
+      )
+    ]
+    .filter(el=>el)
 
   return (
     <AgoraProvider agora={agora}>
       <LiveAV.Provider>
-        {
-        (true || cfgSpaces.length > 1) ?
-          <TabView
-            titles={
-              backstageEnabled ?
-                [<em>backstage</em>].concat(cfgTitles)
-              :
-                cfgTitles
-            }
-            bodies={
-              backstageEnabled ?
-                [<Backstage/>].concat(cfgBodies)
-              :
-                cfgBodies
-            }
-          />
-        :
-          <SpaceProvider space={spaces[0]}>
-            <GatedSpaceFlow/>
-          </SpaceProvider>
-        }
+        <TabView titles={titles} bodies={bodies}/>
+        {/* <SpaceProvider space={spaces[0]}>
+          <GatedSpaceFlow/>
+        </SpaceProvider> */}
       </LiveAV.Provider>
     </AgoraProvider>
   )
