11bd68e0cdf21b434efc640518d02a7aac61ef08
Martin Simpson <martins93@gmail.com>
2023-05-25 07:54:21 +0200

    update mobile styling: tabs scroll horizontally if overflow, info page content wraps

--- src/components/InfoPage.jsx
+++ src/components/InfoPage.jsx
@@ -3,11 +3,11 @@ import { Pad, TablePad } from './Pad'
 export function InfoPage() {
   return (
     <div style={{padding: '30px'}}>
-      <div style={{display: 'flex', justifyContent: 'space-between'}}>
-        <div style={{width: '45vw'}}>
+      <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between'}}>
+        <div style={{minWidth: '200px', maxWidth: '600px'}}>
           <Pad id={`infopage.0`} outsideFlow={true} publicEditable={false}/>
         </div>
-        <div style={{width: '45vw'}}>
+        <div style={{minWidth: '200px', maxWidth: '600px'}}>
           <TablePad id={`infopage.1`} publicEditable={false} />
         </div>
       </div>
