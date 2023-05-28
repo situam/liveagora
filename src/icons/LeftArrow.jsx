4bb5845683968d05177affa0e1aae2797b325eb5
Martin Simpson <martins93@gmail.com>
2023-05-28 07:11:31 +0200

    feature: add backbutton with urlparam:from

---
+++ src/icons/LeftArrow.jsx
@@ -0,0 +1,14 @@
+export default function LeftArrow() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="svg-icon"
+      style={{ width: "1em", height: "1em", verticalAlign: "middle" }}
+      fill="currentColor"
+      overflow="hidden"
+      viewBox="0 0 1024 1024"
+    >
+      <path d="M853.333 469.333v85.334h-512L576 789.333l-60.587 60.587L177.493 512l337.92-337.92L576 234.667 341.333 469.333h512z"></path>
+    </svg>
+  );
+}
