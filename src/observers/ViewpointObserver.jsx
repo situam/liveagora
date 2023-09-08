6063a3d9994bfe3afabd696ed247c9ac28202f2f
martin simpson <martin@taat.live>
2023-09-08 10:26:35 +0200

    add viewport broadcast and follow functionality to broadcast: in console, set window.agoraBroadcastViewport = true to follow: append to url options: &follow=<awarenessId>

---
+++ src/observers/ViewpointObserver.jsx
@@ -0,0 +1,40 @@
+import { useCallback } from 'react';
+import { useOnViewportChange, useReactFlow } from 'reactflow';
+import throttle from 'lodash.throttle'
+import { useAwareness } from "../hooks/useAwareness"
+
+
+
+function onViewportChanged(viewport, updateFn) {
+  /*
+    updateFn is only called if window.agoraBroadcastViewport is set
+    TODO set this
+  */
+  if (window.agoraBroadcastViewport)
+    updateFn(viewport)
+}
+
+const throttleMs = 40
+const onViewportChangedThrottled = throttle(onViewportChanged, throttleMs)
+
+export const ViewpointChangeLogger = () => {
+  const awareness = useAwareness()
+  const { setViewport, viewportInitialized } = useReactFlow();
+
+  const updateAwarenessWithViewport = useCallback((vp)=>awareness.setLocalStateField('viewport', vp), [awareness]);
+
+  useOnViewportChange({
+    onStart: useCallback((viewport)=>onViewportChangedThrottled(viewport, updateAwarenessWithViewport), []),
+    onChange: useCallback((viewport)=>onViewportChangedThrottled(viewport, updateAwarenessWithViewport), []),
+    onEnd: useCallback((viewport)=>onViewportChangedThrottled(viewport, updateAwarenessWithViewport), []),
+  });
+
+  window.setVp = (vp) => {
+    setViewport(vp)
+    console.log("[setVp]", vp)
+  }
+
+  useCallback(()=>console.log("vp init", viewportInitialized), [viewportInitialized])
+
+  return null
+}
