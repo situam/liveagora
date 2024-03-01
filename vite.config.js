290e0ed3772bd1a32fbc0a5f30ef0fe7c87a7610
martin simpson <martin@taat.live>
2024-03-01 20:26:31 +0100

    try setting base app url for next environment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://preview.taat.live/',
+  base: process.env.VITE_APP_URL,
   plugins: [react()],
 })
