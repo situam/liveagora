8b2a1ab7acdc26155272fe8f66b5fb07dd18cef4
Martin Simpson <martins93@gmail.com>
2024-03-10 23:20:34 +0100

    add navigation with queryParams 'tab' and 'node'

---
+++ src/lib/navigate.js
@@ -0,0 +1,14 @@
+/**
+ * Update the browser's URL without reloading the page
+ * @param {string} name
+ * @param {string} value 
+ */
+export function updateUrlParam(name, value) {
+  const currentUrl = new URL(window.location);
+  const searchParams = currentUrl.searchParams;
+
+  searchParams.set(name, value);
+
+  currentUrl.search = searchParams.toString();
+  window.history.pushState({ path: currentUrl.toString() }, '', currentUrl.toString());
+}
