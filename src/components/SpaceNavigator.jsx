db5b8be7c96bc3e282000090329a8de1fa7d9d4b
Martin Simpson <martins93@gmail.com>
2024-03-12 21:03:32 +0100

    SpaceNavigator: show any nodes with metadata

--- src/components/SpaceNavigator.jsx
+++ src/components/SpaceNavigator.jsx
@@ -4,28 +4,30 @@ import { usePan } from '../hooks/usePan'
 import './SpaceNavigator.css'
 import { updateUrlParam, UrlParam } from '../lib/navigate'
 
-const _sortNodeGestureByTitle = (a, b) => a.data?.gesture?.title?.localeCompare(b.data?.gesture?.title, undefined, { sensitivity: 'base' })
-const _sortNodeGestureByDate = (a, b) => a.data?.gesture?.date?.localeCompare(b.data?.gesture?.date, undefined, { sensitivity: 'base' })
+const _sortNodesByTitle = (a, b) => a.data?.title?.localeCompare(b.data?.title, undefined, { sensitivity: 'base' })
+const _sortNodesByDate = (a, b) => a.data?.date?.localeCompare(b.data?.date, undefined, { sensitivity: 'base' })
 
 /**
- * SpaceNavigator as a directory of Gestures
+ * SpaceNavigator as a directory of Nodes with metadata
  * 
- * status: a table of gestures is displayed, sorted by date
+ * status: a table of nodes is displayed, sorted by title
  * 
- * | gesture.title | gesture.date |
+ * | title | date |
  * 
- * clicking on the row of a gesture pans the viewport to that gesture
+ * clicking on a row pans the viewport to that node and updates the url
  */
 export const SpaceNavigator = () => {
   const { ykv } = useSpace()
   const { state } = useYkv(ykv) /** subscribe to space nodes ykv */
   const { panToNode } = usePan()
 
-  const sortedGestures =
+  const selectedSortFn = _sortNodesByTitle // TODO make dynamic
+
+  const filteredNodes =
     Array.from(Object.values(state))
       .map(el=>el.val)
-      .filter(node=>node.data?.gesture)
-      .sort(_sortNodeGestureByDate)
+      .filter(node=>node.data?.title)
+      .sort(selectedSortFn)
 
   const navigateToNode = (node) => {
     updateUrlParam(UrlParam.Node, node.id)
@@ -34,14 +36,20 @@ export const SpaceNavigator = () => {
 
   return (
     <div className="SpaceNavigator">
-      <h2>Gestures</h2>
+      <h2>SpaceNavigator</h2>
       <table>
         <tbody>
+          <tr>
+            <td>title</td>
+            <td>date</td>
+            {/*<td>type</td>*/}
+          </tr>
         {
-          ...sortedGestures.map((node)=>
+          ...filteredNodes.map((node)=>
             <tr onClick={()=>navigateToNode(node)}>
-              <td>{node.data?.gesture.title}</td>
-              <td><em>{node.data?.gesture.date}</em></td>
+              <td>{node.data?.title}</td>
+              <td><em>{node.data?.date}</em></td>
+              {/*<td><em>{node.type}</em></td>*/}
             </tr>
           )
         }
