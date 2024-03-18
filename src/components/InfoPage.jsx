b5813d057c4cdf37ecf113d53ac38dc88f1c0461
Martin Simpson <martins93@gmail.com>
2024-03-18 22:54:40 +0100

    infopage: pad only editable if currentRole.canEdit

--- src/components/InfoPage.jsx
+++ src/components/InfoPage.jsx
@@ -1,11 +1,14 @@
+import { useAccessControl } from '../context/AccessControlContext'
 import { Pad, TablePad } from './Pad'
 
 export function InfoPage() {
+  const { currentRole } = useAccessControl()
+
   return (
     <div style={{padding: '30px'}}>
       <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between'}}>
         <div style={{minWidth: '200px', maxWidth: '600px'}}>
-          <Pad id={`infopage.0`} outsideFlow={true} publicEditable={false}/>
+          <Pad id={`infopage.0`} outsideFlow={true} editable={currentRole.canEdit}/>
         </div>
         <div style={{minWidth: '200px', maxWidth: '600px'}}>
           <TablePad id={`infopage.1`} publicEditable={false} />
