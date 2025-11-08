70ac03b5db1dd1fac3c650413096d79d3e3c566c
situam <martin@situ.am>
2025-11-08 14:38:56 +0100

    server: add getImageUploadUrl route

--- packages/server/routes/index.ts
+++ packages/server/routes/index.ts
@@ -1,6 +1,9 @@
 import type { Hono } from "hono"
 import { env } from "../env.ts"
+import { getImageUploadUrl } from "./getImageUploadUrl/handler.ts"
 
 export function registerRoutes(app: Hono) {
   app.get(`${env.routePrefix}/`, c => c.text("OK"))
+
+  app.get(`${env.routePrefix}/getImageUploadUrl`, getImageUploadUrl)
 }
