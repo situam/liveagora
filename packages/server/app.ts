4bb5d025b61597c70b6ab509ef67ad733cb47bc8
situam <martin@situ.am>
2025-11-08 14:35:54 +0100

    server: add cors

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -4,17 +4,36 @@ import { serve } from "@hono/node-server"
 import { hocuspocus } from "./hocuspocus/index.ts"
 import { registerRoutes } from "./routes/index.ts"
 import { registerWebSockets } from "./ws/hocuspocus-ws.ts"
+import { cors } from "hono/cors"
+import { env } from "./env.ts"
 
 const app = new Hono()
 
 app.use(logger())
+
+// setup CORS middleware
+app.use('*',
+  cors({
+    origin: env.corsOrigin,
+    allowMethods: [
+      'GET',
+      'POST',
+      'OPTIONS'
+    ],
+    allowHeaders: [
+      'Content-Type', 
+      'Authorization'
+    ],
+    credentials: true,
+  })
+)
 
 registerRoutes(app)
 const { injectWebSocket } = registerWebSockets(app, hocuspocus)
 
 const server = serve({
   fetch: app.fetch,
-  port: 3001,
+  port: env.port,
 }, (info) => {
   console.log("Server started", info)
   hocuspocus.hooks('onListen', {
