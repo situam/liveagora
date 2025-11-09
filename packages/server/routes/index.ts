8d40ac1a71c33421fb78db2b1a117b2cab9b3674
situam <martin@situ.am>
2025-11-09 18:21:12 +0100

    server: add getObjectStorageUploadUrl route

--- packages/server/routes/index.ts
+++ packages/server/routes/index.ts
@@ -3,11 +3,13 @@ import { env } from "../env.ts"
 import { getImageUploadUrl } from "./getImageUploadUrl/handler.ts"
 import { getVideoUploadUrl } from "./getVideoUploadUrl/handler.ts"
 import { getHmsRoomToken } from "./getHmsRoomToken/handler.ts"
+import { getObjectStorageUploadUrl } from "./getObjectStorageUploadUrl/handler.ts"
 
 export function registerRoutes(app: Hono) {
   app.get(`${env.routePrefix}/`, c => c.text("OK"))
 
   app.get(`${env.routePrefix}/getImageUploadUrl`, getImageUploadUrl)
   app.get(`${env.routePrefix}/getVideoUploadUrl`, getVideoUploadUrl)
+  app.post(`${env.routePrefix}/getObjectStorageUploadUrl`, getObjectStorageUploadUrl)
   app.post(`${env.routePrefix}/getHmsRoomToken`, getHmsRoomToken)
 }
