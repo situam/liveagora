0e81ad2922245b2ab2ef288d899a3e9bc62f0500
situam <martin@situ.am>
2025-11-09 21:21:58 +0100

    chore(web): remove unused pocketbase dependency

--- packages/web/src/services/pocketbase.js
+++
@@ -1,7 +0,0 @@
-import PocketBase from 'pocketbase';
-//import { pocketbaseUrl } from '../environment'
-const pocketbaseUrl = "https://mitbestimmungsorte.pockethost.io/" //https://hocuspocus.taat.live/pb"
-
-
-export const pb = new PocketBase(pocketbaseUrl);
-pb.autoCancellation(false) // otherwise parallel requests fail, fsee https://github.com/pocketbase/js-sdk#auto-cancellation
