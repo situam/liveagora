9a7b816e859bef9292c0062bc3d2af09d0a2504b
situam <martin@situ.am>
2025-11-16 16:35:58 +0100

    web: specifiy vite base from environment

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,19 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/agora/', //process.env.VITE_APP_URL,
-  plugins: [
-    react(),
-    eslintPlugin({
-      cache: false,
-      include: ['./src/**/*.js', './src/**/*.jsx'],
-      exclude: [],
-    }),
-  ],
-  optimizeDeps: {
-    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
-  },
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  // validate env vars
+  if (!env.VITE_BASE || !env.VITE_BASE.endsWith('/')) {
+    throw new Error("Invalid environment: Missing/invalid VITE_BASE")
+  }
+
+  return defineConfig({
+    base: env.VITE_BASE,
+    plugins: [
+      react(),
+      eslintPlugin({
+        cache: false,
+        include: ['./src/**/*.js', './src/**/*.jsx'],
+        exclude: [],
+      }),
+    ],
+    optimizeDeps: {
+      exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+    },
+  })
+}
