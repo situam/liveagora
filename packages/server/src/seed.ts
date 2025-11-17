2e4568502e88e2322e614c57379b4f5690489c61
situam <martin@situ.am>
2025-11-17 09:22:20 +0100

    fix(seed.ts): use DocumentNames, use env.pathToDb

--- packages/server/src/seed.ts
+++ packages/server/src/seed.ts
@@ -1,19 +1,24 @@
 import { initializeDatabase } from "./auth/db.ts"
 import { getAgoraPasswordsRow, setAgoraPasswordsRow } from "./repo/agoraPasswords.ts"
 import { getSpacePasswordsRow, setSpacePasswordsRow } from "./repo/spacePasswords.ts"
+import { env } from "./env.ts"
+import { DocumentNames } from "@liveagora/common"
 
-await initializeDatabase()
+await initializeDatabase(env.pathToDb)
+
+const agoraId = DocumentNames.buildAgoraDoc("my-agora")
+const spaceId = DocumentNames.buildSpaceDoc("my-agora", "space00")
 
 await setAgoraPasswordsRow({
-    id: "agora/my-agora",
-    read_password: null,
-    edit_password: "agora-edit",
+  id: agoraId,
+  read_password: null,
+  edit_password: "agora-edit",
 })
 
 await setSpacePasswordsRow({
-    id: "space/my-agora/space00",
-    edit_password: "space-edit",
+  id: spaceId,
+  edit_password: "space-edit",
 })
 
-console.log(await getAgoraPasswordsRow("agora/my-agora"))
-console.log(await getSpacePasswordsRow("space/my-agora/space00"))
+console.log(await getAgoraPasswordsRow(agoraId))
+console.log(await getSpacePasswordsRow(spaceId))
