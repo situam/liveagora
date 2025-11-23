de30cf1839bbf73bd5cf3dfee5bd8556d9477f84
situam <martin@situ.am>
2025-11-23 21:06:39 +0100

    web: update changelog, add .txt extension

--- packages/web/src/config/env.ts
+++ packages/web/src/config/env.ts
@@ -1,6 +1,7 @@
 // TODO: better env schema
 
 export const Env = {
+  baseUrl: import.meta.env.VITE_BASE,
   isCommunityVersion: import.meta.env.VITE_IS_COMMUNITY_VERSION === 'true',
   docNamespace: import.meta.env.VITE_DOC_NAMESPACE ?? '',
   // TODO: add other env vars
