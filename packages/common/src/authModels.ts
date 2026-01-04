8424c4b7c3a2ba62f62524415b4ec7ccb5149554
situam <martin@situ.am>
2026-01-04 10:31:09 +0100

    common: switch zod to @hono/zod-openapi

--- packages/common/src/authModels.ts
+++ packages/common/src/authModels.ts
@@ -1,4 +1,4 @@
-import { z } from "zod"
+import { z } from "@hono/zod-openapi"
 
 export const AgoraPasswordsRowSchema = z.object({
   id: z.string(),
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,4 +1,4 @@
-import { z } from "zod"
+import { z } from "@hono/zod-openapi"
 
 export const GetUploadUrlBodySchema = z.object({
   filename: z.string().min(1),
