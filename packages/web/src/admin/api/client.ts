266f70caa7a6609398bd6d5fc571fd9a0f000f3d
situam <martin@situ.am>
2026-01-09 09:15:10 -0300

    server: change AdminAppType to ApiType, add app routes

--- packages/web/src/admin/api/client.ts
+++ packages/web/src/admin/api/client.ts
@@ -1,6 +1,6 @@
 import { hc } from 'hono/client'
-import type { AdminAppType } from '@liveagora/server/src/routes/admin/admin.index.ts'
+import type { ApiType } from '@liveagora/server/src/routes/admin/admin.index.ts'
 import { Env } from '../../config/env'
 
-const client = hc<AdminAppType>(Env.serverUrl)
+const client = hc<ApiType>(Env.serverUrl)
 export const apiClient = client[Env.apiBase]
