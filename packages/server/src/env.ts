e2be55d4ed4703a98343dc789ee942ccb444116c
situam <martin@situ.am>
2026-01-17 08:45:06 -0300

    chore(server): remove cloudflare integration

--- packages/server/src/env.ts
+++ packages/server/src/env.ts
@@ -12,9 +12,6 @@ export const env = {
   port: Number(required('PORT')),
   routePrefix: required('ROUTE_PREFIX'),
   corsOrigin: required('CORS_ORIGIN'),
-  cloudflareImagesAccountId: required('CLOUDFLARE_IMAGES_ACCOUNT_ID'),
-  cloudflareStreamAccountId: required('CLOUDFLARE_STREAM_ACCOUNT_ID'),
-  apiTokenCloudflareStreamImages: required('API_TOKEN_CLOUDFLARE_STREAM_IMAGES'),
   hmsAppAccessKey: required('100MS_APP_ACCESS_KEY'),
   hmsAppSecret: required('100MS_APP_SECRET'),
   s3: {
--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -1,13 +1,9 @@
 import type { OpenAPIHono } from "@hono/zod-openapi"
-import { getImageUploadUrl } from "./getImageUploadUrl/handler.ts"
-import { getVideoUploadUrl } from "./getVideoUploadUrl/handler.ts"
 import { getHmsRoomToken } from "./getHmsRoomToken/handler.ts"
 import { getObjectStorageUploadUrl } from "./getObjectStorageUploadUrl/handler.ts"
 
 export function registerRoutesV1(app: OpenAPIHono) {
   app.get(`/`, c => c.text("OK"))
-  app.get(`/getImageUploadUrl`, getImageUploadUrl)
-  app.get(`/getVideoUploadUrl`, getVideoUploadUrl)
   app.post(`/getObjectStorageUploadUrl`, getObjectStorageUploadUrl)
   app.post(`/getHmsRoomToken`, getHmsRoomToken)
 }
