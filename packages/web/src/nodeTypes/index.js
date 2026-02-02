10105b26dd12af186eaca80b9e071a7c5ab4ec8a
situam <martin@situ.am>
2026-02-02 14:59:40 +0300

    web: add ImagesNode

--- packages/web/src/nodeTypes/index.js
+++ packages/web/src/nodeTypes/index.js
@@ -1,8 +1,10 @@
 import { AgoraNode, ImageNode, VideoNode, SoundNode, PadNode, DemoNode, NodeHatcher, SubspaceNode, StageNode, ScreenShareNode, BoundaryNode } from './nodeTypes';
+import ImagesNode from './ImagesNode';
 import { LocalPeer, RemotePeer } from './PeerNode';
 
 export const nodeTypes = {
   image: ImageNode,
+  images: ImagesNode,
   video: VideoNode,
   sound: SoundNode,
   PadNode,
