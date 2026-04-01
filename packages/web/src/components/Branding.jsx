ce7e0fa6b5424b506b9d0b3a4dac64948a4844d9
situam <martin@situ.am>
2026-04-01 11:40:25 +0200

    tweak(web): update branding dialog: add link to source code

--- packages/web/src/components/Branding.jsx
+++ packages/web/src/components/Branding.jsx
@@ -25,7 +25,8 @@ export function BrandingDialog() {
         a project by <a href="https://taat.live" target="_blank" rel="noreferrer">taat</a>.
         for questions and feedback, email <a href="mailto:agora@taat.live" target="_blank" rel="noreferrer">agora@taat.live</a>. we&apos;d love to learn how you are using it.</p>
         <br/>
-        <p>live agora last updated 2026.02.02, view <a href={`${Env.baseUrl}changelog.txt`} target="_blank" rel="noreferrer">changelog</a></p>
+        {/* eslint-disable-next-line react/jsx-no-target-blank */}
+        <p>live agora last updated 2026.04.01, view <a href={`${Env.baseUrl}changelog.txt`} target="_blank" rel="noreferrer">changelog</a> and <a href='https://github.com/situam/liveagora' target='_blank' rel="noopener">source code</a></p>
         <br/>
         <p><span style={{fontStyle: "italic"}}>initiated by Breg&nbsp;Horemans and Gert-Jan&nbsp;Stam, developed by Martin&nbsp;Simpson, supported by David&nbsp;Martens</span></p>
         
