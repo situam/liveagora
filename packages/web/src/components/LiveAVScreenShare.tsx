2c0b224a9f39e8f987bebf0b59ae55af57b165b9
situam <martin@situ.am>
2026-06-16 11:54:32 +0200

    tweak(web): screenshare background color if no video track

--- packages/web/src/components/LiveAVScreenShare.tsx
+++ packages/web/src/components/LiveAVScreenShare.tsx
@@ -21,7 +21,7 @@ export function LiveAVScreenShare({ data }: {data: AwarenessScreenshareState["da
 
   if (!screenshareVideoTrack)
     return <div
-      style={{width:'100%', height:'100%', background: 'rgba(255,255,255,0.5)', opacity: 0.5, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      style={{width:'100%', height:'100%', background: '#000'}}>
     </div>
 
   return (
