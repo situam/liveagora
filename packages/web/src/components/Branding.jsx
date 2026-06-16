bf500a02dd940da0301f487e9bf2b9eb410c9bfe
situam <martin@situ.am>
2026-06-16 11:55:52 +0200

    docs(web): update changelog

--- packages/web/src/components/Branding.jsx
+++ packages/web/src/components/Branding.jsx
@@ -26,7 +26,7 @@ export function BrandingDialog() {
         for questions and feedback, email <a href="mailto:agora@taat.live" target="_blank" rel="noreferrer">agora@taat.live</a>. we&apos;d love to learn how you are using it.</p>
         <br/>
         {/* eslint-disable-next-line react/jsx-no-target-blank */}
-        <p>live agora last updated 2026.06.15, view <a href={`${Env.baseUrl}changelog.txt`} target="_blank" rel="noreferrer">changelog</a> and <a href='https://github.com/situam/liveagora' target='_blank' rel="noopener">source code</a></p>
+        <p>live agora last updated 2026.06.16, view <a href={`${Env.baseUrl}changelog.txt`} target="_blank" rel="noreferrer">changelog</a> and <a href='https://github.com/situam/liveagora' target='_blank' rel="noopener">source code</a></p>
         <br/>
         <p><span style={{fontStyle: "italic"}}>initiated by Breg&nbsp;Horemans and Gert-Jan&nbsp;Stam, developed by Martin&nbsp;Simpson, supported by David&nbsp;Martens</span></p>
         
